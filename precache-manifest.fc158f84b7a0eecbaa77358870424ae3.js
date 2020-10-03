5fe31b3a5f9ea6f0ddbbced4651b5ba51549ac5d
prathyusha-padmanabhuni <prathyushasindhusha@gmail.com>
2020-10-03 18:29:59 +0530

    Updates

---
+++ precache-manifest.fc158f84b7a0eecbaa77358870424ae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14c83848308c6a49bbe91597cef8bae",
+    "url": "/reactproject.git/index.html"
+  },
+  {
+    "revision": "594e66eddaeef8522515",
+    "url": "/reactproject.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74361ce84ba14033f909",
+    "url": "/reactproject.git/static/js/2.f25df036.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactproject.git/static/js/2.f25df036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594e66eddaeef8522515",
+    "url": "/reactproject.git/static/js/main.85d62576.chunk.js"
+  },
+  {
+    "revision": "c1d326a386c16f97dd46",
+    "url": "/reactproject.git/static/js/runtime-main.c4f0a272.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactproject.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
